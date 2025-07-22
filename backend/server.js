c503f295c1f99fa366bd0e7a29ac9eec805faeb3
sriyaT <tsriya6@gmail.com>
2025-07-22 21:28:10 +0530

    Initial setup: Backend + Frontend

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+// Example Socket.io connection
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Chat App Backend is running");
+});
+
+server.listen(5000, () => {
+  console.log("Backend server running on http://localhost:5000");
+});
